ebf81731fca26e321ee685807558d12c7454d618
Abstract-X <abstract-x-mail@protonmail.com>
2021-02-28 11:58:53 +1100

    Change approach to using third-party JSON serialization/deserialization functions

--- tgbotcallback/tgbotcallback.py
+++ tgbotcallback/tgbotcallback.py
@@ -1,17 +1,42 @@
 import json as jsonlib
-from typing import Union, Tuple, Callable
+from typing import Union, Tuple, Callable, List
 
 from . import exceptions
 
 
 FilterType = Union[str, int]
 ValueType = Union[str, int, float, bool, list, None]
+NonSerializedDataType = Union[FilterType, List[Union[FilterType, ValueType]]]
 
 DATA_LENGTH_LIMIT = 64  # taken from https://core.telegram.org/bots/api#inlinekeyboardbutton
 
 
+def _serialize_data(data: NonSerializedDataType) -> str:
+    """
+    Serialize data to string.
+
+    Provides compactness of the string due to the absence of spaces.
+
+    :param data: callback data parts.
+    :return: serialized data string.
+    """
+
+    return jsonlib.dumps(data, separators=(",", ":"))
+
+
+def _deserialize_data(data: str) -> NonSerializedDataType:
+    """
+    Deserialize string to data.
+
+    :param data: serialized data string.
+    :return: callback data parts.
+    """
+
+    return jsonlib.loads(data)
+
+
 def make_data(filter_: FilterType, value: ValueType = None, *,
-              json_serializer: Callable = jsonlib.dumps) -> str:
+              json_serializer: Callable[[NonSerializedDataType], str] = _serialize_data) -> str:
     """
     Make a string for callback_data field in InlineKeyboardButton.
 
@@ -22,14 +47,7 @@ def make_data(filter_: FilterType, value: ValueType = None, *,
     :return: callback_data string.
     """
 
-    data = [filter_, value] if value is not None else filter_
-    try:
-        data = json_serializer(data, separators=(",", ":"))
-    except TypeError as exception:  # if there is no separators kwarg in the serializer
-        if exception.args[0] == "'separators' is an invalid keyword argument for this function":
-            data = json_serializer(data)
-        else:
-            raise
+    data = json_serializer([filter_, value] if value is not None else filter_)
 
     data_length = len(data.encode())
     if data_length > DATA_LENGTH_LIMIT:
@@ -38,7 +56,9 @@ def make_data(filter_: FilterType, value: ValueType = None, *,
     return data
 
 
-def parse_data(data: str, *, json_deserializer: Callable = jsonlib.loads) -> Tuple[FilterType, ValueType]:
+def parse_data(data: str, *,
+               json_deserializer: Callable[[str], NonSerializedDataType] =
+               _deserialize_data) -> Tuple[FilterType, ValueType]:
     """
     Parse data to get filter value and button value.
 
